b83258b7ed806a747b59150b0471e6dfe2b77327
katherinebui <kathy.codes@gmail.com>
2017-11-12 15:15:58 -0800

    Subtract, multiple, and divide works

--- problem-3/calculator.js
+++ problem-3/calculator.js
@@ -7,6 +7,21 @@ Calculator.prototype.add = function(first_num, second_num) {
   return first_num + second_num;
 };
 
+Calculator.prototype.subtract = function(first_num, second_num) {
+  return first_num - second_num;
+};
+
+Calculator.prototype.multiple = function(first_num, second_num) {
+  return first_num * second_num;
+};
+
+Calculator.prototype.divide = function(first_num, second_num) {
+  return first_num/second_num;
+};
+
 calculator = new Calculator();
-console.log(calculator);
-console.log(calculator.add(1,2));
+// console.log(calculator);
+// console.log(calculator.add(1,2));
+console.log(calculator.subtract(7,2));
+console.log(calculator.multiple(4,6));
+console.log(calculator.divide(22,11))

b1158466004afa2da8ccbed9415d014bc4234203
katherinebui <kathy.codes@gmail.com>
2017-11-12 15:15:30 -0800

    Add works

--- problem-3/calculator.js
+++ problem-3/calculator.js
@@ -6,3 +6,7 @@ function Calculator(first_num, second_num) {
 Calculator.prototype.add = function(first_num, second_num) {
   return first_num + second_num;
 };
+
+calculator = new Calculator();
+console.log(calculator);
+console.log(calculator.add(1,2));

5788d73aa2c0a314b04912c44e014e4b7e62845c
katherinebui <kathy.codes@gmail.com>
2017-11-12 17:09:08 -0800

    Add new column for removal of item

--- problem-1/js/script.js
+++ problem-1/js/script.js
@@ -1,18 +1,19 @@
 function addItem() {
   var table = document.querySelector('.table');
-  var row = table.insertRow(1);
+  var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
   cell1.innerHTML = document.getElementById('name').value;
   cell2.innerHTML = document.getElementById('topic').value;
   cell3.innerHTML = document.getElementById('due').value;
-  cell4.innerHTML = document.getElementById('completed').value + ;
+  cell4.innerHTML = document.getElementById('completed').value;
+  cell5.innerHTML = 'X';
 
   document.getElementById('name').value = '';
   document.getElementById('topic').value = '';
   document.getElementById('due').value = '';
   document.getElementById('completed').value = '';
 }
-

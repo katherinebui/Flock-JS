e67544cb9872b28352da892169934bcef3b45d71
katherinebui <kathy.codes@gmail.com>
2017-11-12 17:31:59 -0800

    Finally register hi when clicking x

--- problem-1/js/script.js
+++ problem-1/js/script.js
@@ -1,5 +1,20 @@
+function removeItem() {
+  var close = document.getElementsByClassName('close');
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      console.log('hi');
+    };
+  }
+}
+
 function addItem() {
   var table = document.querySelector('.table');
+  var button = document.createElement('button');
+  button.className = 'close';
+  var x = document.createTextNode('\u00D7');
+  button.appendChild(x);
+  button.setAttribute('onclick', 'removeItem()');
   var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
@@ -10,7 +25,7 @@ function addItem() {
   cell2.innerHTML = document.getElementById('topic').value;
   cell3.innerHTML = document.getElementById('due').value;
   cell4.innerHTML = document.getElementById('completed').value;
-  cell5.innerHTML = 'X';
+  cell5.appendChild(button);
 
   document.getElementById('name').value = '';
   document.getElementById('topic').value = '';

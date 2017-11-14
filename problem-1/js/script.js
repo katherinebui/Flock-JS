60f4a1f6e630dac0894a9bb5f54583143bc3964a
katherinebui <kathy.codes@gmail.com>
2017-11-13 21:33:22 -0800

    Refactor more js

--- problem-1/js/script.js
+++ problem-1/js/script.js
@@ -1,7 +1,6 @@
 function removeItem() {
-  var table = document.getElementById('tbody');
-  var close = document.getElementsByClassName('close');
-  var lastListItem = table.lastChild;
+  var tableBody = document.getElementById('tbody');
+  var lastListItem = tableBody.lastChild;
   lastListItem.remove();
 }
 
@@ -13,15 +12,15 @@ function addItem() {
   button.appendChild(x);
   button.setAttribute('onclick', 'removeItem()');
   var row = table.insertRow(-1);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
-  cell1.innerHTML = document.getElementById('name').value;
-  cell2.innerHTML = document.getElementById('topic').value;
-  cell3.innerHTML = document.getElementById('due').value;
-  cell4.innerHTML = document.getElementById('checkbox').checked;
-  cell5.appendChild(button);
+  var cell0 = row.insertCell(0);
+  var cell1 = row.insertCell(1);
+  var cell2 = row.insertCell(2);
+  var cell3 = row.insertCell(3);
+  var cell4 = row.insertCell(4);
+  cell0.innerHTML = document.getElementById('name').value;
+  cell1.innerHTML = document.getElementById('topic').value;
+  cell2.innerHTML = document.getElementById('due').value;
+  cell3.innerHTML = document.getElementById('checkbox').checked;
+  cell4.appendChild(button);
   document.querySelector('form').reset();
 }
